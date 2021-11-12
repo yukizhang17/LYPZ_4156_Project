fed8b2f3f572ff3a23432217569e97ea749baf1e
JonasanL <36778407+JonasanL@users.noreply.github.com>
2021-11-12 15:48:30 -0500

    update username format, and create generate_apikey

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, g
 from authlib.integrations.flask_client import OAuth
 from auth0.v3.authentication import GetToken
 from auth0.v3.authentication import Users
@@ -8,25 +8,60 @@ import uuid
 import json
 import requests
 import socket
+import sqlite3
+
 
 app = Flask(__name__)
 
 oauth = OAuth(app)
 
 CLIENT_ID = 'EfQZGs8qdAdrof7gkCU7hMN12M5yMi3G'
-CLIENT_SECRET='z-CiE8aGv75UMqTjZZf_Cmbs3hraHNVhvKn92fMxpMl1FBm6kW5wZMK06Qk5W9Hc'
-API='https://4156_LYPZ/api'
-DB='Username-Password-Authentication'
+CLIENT_SECRET = 'z-CiE8aGv75UMqTjZZf_Cmbs3hraHNVhvKn92fMxpMl1FBm6kW5wZMK06Qk5W9Hc'
+API = 'https://4156_LYPZ/api'
+DB = 'Username-Password-Authentication'
+DATABASE = './db/LYPZ.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/', methods=['GET'])
+def home():
+    for user in query_db("select * from user where password = 'mypass'"):
+        print(user)
+
+    return "A"
+
+
+
+
+
+#Generate 32 digit of unique token for each dev
+@app.route('/generate-apikey', methods=['POST'])
+def generate_apikey():
+    token = uuid.uuid4().hex
+
+    return {"api_key": token}
+    
 
 @app.route('/signup', methods=['POST'])
 def signup():
     form = request.form
     email = form["email"]
-    username = email + "Yuki"
+    #emaillist = email.split("@")
     password = form["password"]
-
-    db = Database('dev-ntceedrk.us.auth0.com')
-    db.signup(client_id=CLIENT_ID, username = username, email=email, password=password, connection='Username-Password-Authentication')
+    api_key = form["api_key"]
+    username = email + "_apikey_" + api_key
+    #username = emaillist[0] + emaillist[1].split(".")[0] + emaillist[1].split(".")[1] 
 
     data = {
         "client_id": CLIENT_ID,
@@ -40,12 +75,13 @@ def signup():
 
     return jsonify(callback.json())
 
-
 @app.route('/login', methods=['GET'])
 def login():
     form = request.form
     email = form["email"]
-    username = email + "Yuki"
+    #emaillist = email.split("@")
+    api_key = form["api_key"]
+    username = email + "_apikey_" + api_key
     password = form["password"]
 
     token = GetToken('dev-ntceedrk.us.auth0.com')
